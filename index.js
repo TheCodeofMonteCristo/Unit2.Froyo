aaec014124e6d76b6f57c7c493f78ab971063828
Shane A. Healy <ShaneAHealy55@gmail.com>
2024-09-12 01:46:12 -0500

    Froyo

--- index.js
+++ index.js
@@ -6,8 +6,45 @@ In this case, they will be able to observe that they have ordered three vanilla,
 */
 
 
+const froyoFlavorMachine = prompt(
+    "Please enter the Froyo flavors you would like to order seperated by commas.", "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  );
+
+console.log('We all enjoy Ice Cream and Frozen Yogurt');
+
+//Receive Input
+
+
+const selectedFlavors = froyoFlavorMachine.split(',');
+console.log(selectedFlavors);
+
+//Transform to an Array of Strings 
 
 
 
 
+/**
+ * 
+ * @param {array} bestTasting 
+ * @returns {object} 
+ */
 
+function findFroyo(bestTasting) {
+    const iceCream = {};
+    for (let i = 0; i < bestTasting.length; i++) {
+        let froyoVisit = bestTasting[i];
+        if (froyoVisit in iceCream) {
+            flavors[froyoVisit] += 1;
+        } else {
+            iceCream[froyoVisit] = 1;
+        }
+    }
+    return iceCream;
+}
+
+console.log(findFroyo(selectedFlavors));
+
+
+
+
+//
